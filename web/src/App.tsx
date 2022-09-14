8985c044ca5f47e5fb7973fa3f4498877761a412
ViniDevBR <vinigtr386@gmail.com>
2022-09-13 23:30:08 -0300

    :fire: New front-End Interface

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,80 @@
-import './styles/main.css'
+import {MagnifyingGlassPlus} from 'phosphor-react';
+import './styles/main.css';
+import logo from './assets/Logo.svg';
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className='max-w-[1344px] mx-auto flex items-center flex-col my-20'>
+      <img src={logo} alt="logo rocketseat" />
+      <h1 className='text-6xl text-white font-black mt-20'>Seu 
+      <span className='bg-gradiente bg-clip-text text-transparent'> duo </span> 
+      esta aqui</h1>
+
+      <div className= 'grid grid-cols-6 gap-6 mt-16 rounded-lg overflow-hidden'>
+        <a href="" className= "relative rounded-lg overflow-hidden">
+          <img src="../public/image 1.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 Anuncios</span>
+          </div>
+        </a>
+
+        <a href="" className= "relative rounded-lg overflow-hidden">
+          <img src="../public/image 2.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block'>4 Anuncios</span>
+          </div>
+        </a>
+
+        <a href="" className= "relative rounded-lg overflow-hidden">
+          <img src="../public/image 3.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 Anuncios</span>
+          </div>
+        </a>
+
+        <a href="" className= "relative rounded-lg overflow-hidden">
+          <img src="../public/image 5.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white'>Apex</strong>
+            <span className='text-zinc-300 text-sm block'>4 Anuncios</span>
+          </div>
+        </a>
+
+        <a href="" className= "relative rounded-lg overflow-hidden">
+          <img src="../public/image 6.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block'>4 Anuncios</span>
+          </div>
+        </a>
+
+        <a href="" className= "relative rounded-lg overflow-hidden">
+          <img src="../public/image 7.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
+            <strong className='font-bold text-white'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block'>4 Anuncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className='pt-1 mt-8 self-stretch bg-gradiente rounded-lg'>
+        <div className='bg-[#2A2634] py-6 px-8 self-stretch flex justify-between items-center'>
+          <div>
+            <strong className='text-2x1 text-white font-black'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anuncio para encontrar novos players!</span>
+          </div>
+          
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24}/>
+            Publicar Anuncio
+          </button>
+        </div>
+      </div>
+    </div>
+  ) 
 }
 
 export default App
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradiente':
+          'linear-gradient(89.86deg, #9572FC 15.00%, #43E7AD 63.00%, #E1D55D 15.00%);',
+        galaxy: "url('../public/Fundo.png')",
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
