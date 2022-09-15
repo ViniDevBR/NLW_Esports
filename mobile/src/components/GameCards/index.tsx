6d20d3da851a874b512299dad15b593532d7f272
ViniDevBR <vinigtr386@gmail.com>
2022-09-14 22:21:17 -0300

    :fire: More components

---
+++ mobile/src/components/GameCards/index.tsx
@@ -0,0 +1,11 @@
+import { TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+
+export function GameCards() {
+  return (
+    <TouchableOpacity style={styles.container}>
+
+    </TouchableOpacity>
+  );
+}
--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -1,10 +1,19 @@
 import { styles } from './styles';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+import { logoImg } from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 export function Home(){
   return (
     <View style={styles.container}>
-
+      <Image 
+      source={logoImg}
+      style={styles.logo}
+      />
+      <Heading
+      title='Encontre seu duo'
+      subtitle='Selecione o game que deseja jogar...'
+      />
     </View>
   )
 }
