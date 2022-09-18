308b333dde87660982001d9f5ff0b9d998a33b44
ViniDevBR <vinigtr386@gmail.com>
2022-09-17 23:56:33 -0300

    :fire: Adding components

---
+++ web/src/components/CreateAdBanner.tsx
@@ -0,0 +1,23 @@
+import {MagnifyingGlassPlus} from 'phosphor-react';
+
+export function CreateAdBanner() {
+  return (
+    <div className="pt-1 mt-8 self-stretch bg-gradiente rounded-lg">
+      <div className="bg-[#2A2634] py-6 px-8 self-stretch flex justify-between items-center">
+        <div>
+          <strong className="text-2x1 text-white font-black">
+            Não encontrou seu duo?
+          </strong>
+          <span className="text-zinc-400 block">
+            Publique um anuncio para encontrar novos players!
+          </span>
+        </div>
+
+        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+          <MagnifyingGlassPlus size={24} />
+          Publicar Anuncio
+        </button>
+      </div>
+    </div>
+  )
+}
