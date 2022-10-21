292951e7708c814947787b8f7d09a1491cd677d3
ViniDevBR <vinigtr386@gmail.com>
2022-10-21 15:32:50 -0300

    :fire: New component for weekDays

---
+++ web/src/components/Form/ButtonDays.tsx
@@ -0,0 +1,89 @@
+//RADIX UI
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+
+//REACT
+import { useState } from 'react'
+
+export function ButtonDays() {
+  const [day, setDay] = useState<string[]>([])
+
+  return (
+    <ToggleGroup.Root
+      type="multiple"
+      className="grid grid-cols-4 gap-2"
+      onValueChange={setDay}
+    >
+
+      <ToggleGroup.Item
+        value="0"
+        className={`w-8 h-8 rounded ${
+          day.includes('0') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Domingo"
+      >
+        D
+      </ToggleGroup.Item>
+
+      <ToggleGroup.Item
+        value="1"
+        className={`w-8 h-8 rounded ${
+          day.includes('1') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Segunda"
+      >
+        S
+      </ToggleGroup.Item>
+
+      <ToggleGroup.Item
+        value="2"
+        className={`w-8 h-8 rounded ${
+          day.includes('2') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Terça"
+      >
+        T
+      </ToggleGroup.Item>
+
+      <ToggleGroup.Item
+        value="3"
+        className={`w-8 h-8 rounded ${
+          day.includes('3') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Quarta"
+      >
+        Q
+      </ToggleGroup.Item>
+
+      <ToggleGroup.Item
+        value="4"
+        className={`w-8 h-8 rounded ${
+          day.includes('4') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Quinta"
+      >
+        Q
+      </ToggleGroup.Item>
+
+      <ToggleGroup.Item
+        value="5"
+        className={`w-8 h-8 rounded ${
+          day.includes('5') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Sexta"
+      >
+        S
+      </ToggleGroup.Item>
+
+      <ToggleGroup.Item
+        value="6"
+        className={`w-8 h-8 rounded ${
+          day.includes('6') ? 'bg-violet-500' : 'bg-zinc-900'
+        }`}
+        title="Sabado"
+      >
+        S
+      </ToggleGroup.Item>
+
+    </ToggleGroup.Root>
+  )
+}
